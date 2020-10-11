1e8632e74c1b6bee7e62de7a8e8d44db927e0b9e
John Michael Bell <johnmichaelbell@gmail.com>
2020-10-11 13:22:55 -0400

    renamed from egui_wgpu to egui_winit_wgpu

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![MIT](https://img.shields.io/badge/license-MIT-blue.svg)
 ![Apache](https://img.shields.io/badge/license-Apache-blue.svg)
 
-# egui_wgpu
+# egui_winit_wgpu
 
 This crates provides bindings between [Egui](https://crates.io/crates/egui) and [wgpu](https://crates.io/crates/wgpu).
--- examples/demo.rs
+++ examples/demo.rs
@@ -1,12 +1,12 @@
 #![deny(warnings)]
 #![warn(clippy::all)]
 
-use egui_wgpu::{storage::FileStorage, RunMode};
+use egui_winit_wgpu::{storage::FileStorage, RunMode};
 
 fn main() {
     wgpu_subscriber::initialize_default_subscriber(None);
     let title = "Egui wgpu demo";
     let storage = FileStorage::from_path(".egui_demo_wgpu.json".into());
     let app: egui::DemoApp = egui::app::get_value(&storage, egui::app::APP_KEY).unwrap_or_default();
-    egui_wgpu::run(title, RunMode::Reactive, storage, app);
+    egui_winit_wgpu::run(title, RunMode::Reactive, storage, app);
 }
